8f90239977b1af2d6ffce4bca8927537ace9d65e
nemonemonee <88709397+nemonemonee@users.noreply.github.com>
2021-08-23 18:50:03 -0700

    Update create_sample_code.sh

--- create_sample_code.sh
+++ create_sample_code.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
 sudo docker run -it --rm --name hello-world-project -v "$(pwd)":"$(pwd)" -w "$(pwd)" maven:latest mvn archetype:generate -DarchetypeGroupId=io.dropwizard.archetypes -DarchetypeArtifactId=java-simple -DarchetypeVersion=2.0.24
-io.dropwizard.archetypes
-helloworldDemo
-0.0.1
-com.example.helloworld
-HelloWorld
