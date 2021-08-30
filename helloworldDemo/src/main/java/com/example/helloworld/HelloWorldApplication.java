da03216503b43d5bcccbe25d2345c400c1f3a4d3
nemonemonee <88709397+nemonemonee@users.noreply.github.com>
2021-08-30 00:02:23 -0700

    Update HelloWorldApplication.java

--- helloworldDemo/src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ helloworldDemo/src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -24,9 +24,12 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration> 
     public void run(HelloWorldConfiguration configuration,
                     Environment environment) {
         final HelloWorldResource resource = new HelloWorldResource(
-            configuration.getTemplate(),
-            configuration.getDefaultName()
-            );
+        configuration.getTemplate(),
+        configuration.getDefaultName()
+        );
+        final TemplateHealthCheck healthCheck =
+            new TemplateHealthCheck(configuration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);
         environment.jersey().register(resource);
     }
 
