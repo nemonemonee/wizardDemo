7c3048eff7b21cf80cd8eb89cfd3795c65244a5f
nemonemonee <88709397+nemonemonee@users.noreply.github.com>
2021-09-01 23:09:56 -0700

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd helloworldDemo
-sudo docker run -it --rm --name hello-world-project -v "$(pwd)":"$(pwd)" -w "$(pwd)" maven:latest  java -jar target/helloworldDemo-0.0.1.jar server config.yml
+sudo docker run -it --rm --name hello-world-project --network="host" -v "$(pwd)":"$(pwd)" -w "$(pwd)" maven:latest  java -jar target/helloworldDemo-0.0.1.jar server hello_world.yml
 cd ..
